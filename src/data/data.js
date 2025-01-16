b8c228d3e70adf80a54bec7e571d101fb72f46b8
rrachelnaomi <rachel.bekker@ucalgary.ca>
2025-01-16 14:55:37 -0700

    ur message

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,13 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Rachel Bekker",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "rachel.bekker@ucalgary.ca",
+        phone: "587-220-4465",
         linkedin: "https://www.linkedin.com/",
-        github: "https://www.github.com/",
+        github: "https://github.com/rrachelnaomi",
     }
 }
 
