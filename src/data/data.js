bb5e97f7d7d00328fe564a260139ca7c2bcc949c
rrachelnaomi <rachel.bekker@ucalgary.ca>
2025-01-20 20:42:43 -0700

    ur message

--- src/data/data.js
+++ src/data/data.js
@@ -12,31 +12,3 @@ const data = {
 }
 
 export default data;
-function ContactInfo() {
-    return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.bio}</p>
-        <div>
-          <p>
-            Email: <a href={`mailto:${data.contact.email}`}>{data.contact.email}</a>
-          </p>
-          <p>Phone: {data.contact.phone}</p>
-          <p>
-            LinkedIn:{" "}
-            <a href={data.contact.linkedin} target="_blank" rel="noopener noreferrer">
-              Rachel's LinkedIn
-            </a>
-          </p>
-          <p>
-            GitHub:{" "}
-            <a href={data.contact.github} target="_blank" rel="noopener noreferrer">
-              Rachel's GitHub
-            </a>
-          </p>
-        </div>
-      </div>
-    );
-  }
-  
-  export default ContactInfo;
