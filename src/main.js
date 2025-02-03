dbb9746193f6dfd9245e4af40e19bd9b37a99c81
rrachelnaomi <rachel.bekker@ucalgary.ca>
2025-02-03 13:23:58 -0700

    ur message

--- src/main.js
+++ src/main.js
@@ -118,5 +118,5 @@ document.querySelector("#app").innerHTML = `
 
 // Create three.js scenes for each
 createThreeScene("#model1", "/3DModels/project1/cube.obj");
-createThreeScene("#model2", "/3DModels/project2/websquirt.obj");
+createThreeScene("#model2", "/3DModels/project2/squirtWEB.obj");
 createThreeScene("#model3", "/3DModels/project3/cottage.obj");
